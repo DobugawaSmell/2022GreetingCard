14feb87e25cba10e8f7f5184f42f8d5fbfcdc9b8
DobugawaSmell <76649989+DobugawaSmell@users.noreply.github.com>
2021-11-17 22:15:29 +0900

    PCスタイル追加

--- js/backGroundRender.js
+++ js/backGroundRender.js
@@ -1,12 +1,20 @@
 
 // ページの読み込みを待つ
 window.addEventListener('load', ThreeInit);
-window.addEventListener("scroll", scroll);
+window.addEventListener('resize', Resize);
+window.addEventListener('click', CamAction);
 
 //3D用変数
+let width;
+let height;
+let renderer;
+let camera;
 let OPACITY = 0.9;
 let TRANS = true;
 let DEPTH_TEST = false;
+let posX;
+let posY;
+let posZ;
 
 //カメライージング
 let camX_easing = 0;
@@ -14,17 +22,40 @@ let camY_easing = -300;
 let camZ_easing = 0;
 
 //3D用変数
-const radian = 1000;
+let radian = 1000;
 let leftTime = 0;
 
-function ThreeInit() {
+function Resize() {
+  // サイズを指定
+  if(document.documentElement.clientWidth >= 800){
+    width = document.documentElement.clientWidth*0.8;
+  }else{
+    width = document.documentElement.clientWidth
+  }
+  height = document.documentElement.clientHeight*0.8;
 
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(width, height);
+
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+}
+
+function CamAction()
+{
+  radian = Math.random() * 1500;
+  leftTime += Math.random() * 200;
+  posY = Math.random() * 300 + 200;
+}
+
+function ThreeInit() 
+{
     // サイズを指定
-    const width = document.documentElement.clientWidth;
-    const height = document.documentElement.clientHeight;
+    width = document.documentElement.clientWidth*0.8;
+    height = document.documentElement.clientHeight*0.8;
   
     // レンダラーを作成
-    const renderer = new THREE.WebGLRenderer({
+    renderer = new THREE.WebGLRenderer({
       canvas: document.querySelector('#topCanvas'),
       antialias: true,
       alpha: true,  /// <= これ
@@ -37,7 +68,7 @@ function ThreeInit() {
     const scene = new THREE.Scene();
   
     // カメラを作成
-    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera = new THREE.PerspectiveCamera(45, width / height);
     camera.position.set(camX_easing, camY_easing, camZ_easing);
     camera.rotation.set(0.8,0,0);
 
@@ -66,23 +97,24 @@ function ThreeInit() {
         }
     );
 
-    const light = new THREE.HemisphereLight(0xFFFFFF, 0x7F7F7F, 2.0);
-    light.position.set(0,1000,0);
+    const light = new THREE.HemisphereLight(0xFFFFFF, 0x7F7F7F, 1.4);
+    light.position.set(0,300,0);
     scene.add(light);
     
     //背景色変更
-    scene.background = new THREE.Color("#44966D");
+    // scene.background = new THREE.Color("#44966D");
   
     tick();
   
     // 毎フレーム時に実行されるループイベントです
     function tick() {
-      leftTime += 0.005;
-      let posX = radian * Math.sin(leftTime);
-      let posZ = radian * Math.cos(leftTime);
+      leftTime += 0.01;
+      posX = radian * Math.sin(leftTime);
+      posY = 485;
+      posZ = radian * Math.cos(leftTime);
 
       camX_easing += (posX - camX_easing) * 0.1;
-      camY_easing += (485 - camY_easing) * 0.1;
+      camY_easing += (posY - camY_easing) * 0.1;
       camZ_easing += (posZ - camZ_easing) * 0.1;
         
         camera.position.x = camX_easing;
